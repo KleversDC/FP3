5694c0e3ca6b910ac7cf75e38ea7b15a2bee3f7a
KleversDS <klevers170@gmail.com>
2024-10-01 00:03:28 +0300

    Добавьте файлы проекта.

---
+++ FP3/FP3.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {//1
+//    int seconds;
+//    cout << "Enter the number of seconds since the start of the day: ";
+//    cin >> seconds;
+//
+//    int hours = seconds / 3600;
+//    int minutes = (seconds % 3600) / 60;
+//    int remaining_seconds = seconds % 60;
+//
+//    int total_seconds_in_day = 24 * 3600;
+//    int seconds_to_midnight = total_seconds_in_day - seconds;
+//    int hours_left = seconds_to_midnight / 3600;
+//    int minutes_left = (seconds_to_midnight % 3600) / 60;
+//    int seconds_left = seconds_to_midnight % 60;
+//
+//    cout << "Current time: " << hours << " hours, " << minutes << " minutes, " << remaining_seconds << " seconds." << endl;
+//    cout << "Time remaining until midnight: " << hours_left << " hours, " << minutes_left << " minutes, " << seconds_left << " seconds." << endl;
+//    
+////2
+//    double diameter;
+//    const double PI = 3.14159;
+//
+//    cout << "Enter the diameter of the circle: ";
+//    cin >> diameter;
+//
+//    double radius = diameter / 2;
+//    double area = PI * radius * radius;
+//    double perimeter = 2 * PI * radius;
+//
+//    cout << "Area of the circle: " << area << endl;
+//    cout << "Perimeter of the circle: " << perimeter << endl;
+
+//3
+    //double num1, num2;
+    //cout << "Enter two numbers: ";
+    //cin >> num1 >> num2;
+
+    //double sum = num1 + num2;
+    //double product = num1 * num2;
+    //double average = sum / 2;
+
+    //cout << "Sum: " << sum << endl;
+    //cout << "Product: " << product << endl;
+    //cout << "Average: " << average << endl;
+
+
+//4
+    //double num1, num2, num3;
+    //cout << "Enter three numbers: ";
+    //cin >> num1 >> num2 >> num3;
+
+    //double sum = num1 + num2 + num3;
+    //double product = num1 * num2 * num3;
+    //double average = sum / 3;
+
+    //cout << "Sum: " << sum << endl;
+    //cout << "Product: " << product << endl;
+    //cout << "Average: " << average << endl;
+
+//5
+    //double price, discount;
+    //int quantity;
+
+    //cout << "Enter the price of one laptop: ";
+    //cin >> price;
+    //cout << "Enter the quantity of laptops: ";
+    //cin >> quantity;
+    //cout << "Enter the discount percentage: ";
+    //cin >> discount;
+
+    //double total_price = price * quantity;
+    //double discount_amount = total_price * (discount / 100);
+    //double final_price = total_price - discount_amount;
+
+    //cout << "Total price after discount: $" << final_price << endl;
+
+//6
+    //double sales;
+    //cout << "Enter the total sales made by the manager: ";
+    //cin >> sales;
+
+    //double base_salary = 100;
+    //double commission = sales * 0.05;
+    //double total_salary = base_salary + commission;
+
+    //cout << "Manager's total salary: $" << total_salary << endl;
+
+
+//7
+    //double file_size_gb, internet_speed_bps;
+    //cout << "Enter the file size in gigabytes: ";
+    //cin >> file_size_gb;
+    //cout << "Enter the internet speed in bits per second: ";
+    //cin >> internet_speed_bps;
+
+    //double file_size_bits = file_size_gb * 8 * 1e9;
+
+    //double download_time_seconds = file_size_bits / internet_speed_bps;
+
+    //int hours = download_time_seconds / 3600;
+    //int minutes = (int(download_time_seconds) % 3600) / 60;
+    //int seconds = int(download_time_seconds) % 60;
+
+    //cout << "Time to download the file: " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds." << endl;
+}
